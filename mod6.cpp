3750b0e9ee214d89aac1e510be65eee7a6cb8a05
Akshata Bhosale <bhosaleakshata726@gmail.com>
2025-02-12 21:43:44 +0530

    Add files via upload

---
+++ mod6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0, i = 0;
+    
+    printf("Enter a string: ");
+    gets(_); // Fill in the blank
+
+
+/*write code here to count the length of string*/
+    
+    printf("Length of the string: %d\n", _________); // Fill in the blank
+    
+    
+    return 0;
+}
+
---
+++ mod7.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], temp;
+    int i, j;
+    
+    printf("Enter a string: ");
+    gets(_________); // Fill in the blank
+
+    for (//complete for loop*/);  // Find length
+
+    for (i = 0, j = j - 1; i < j; i++, j--) {
+        /*complete the code*/
+    }
+    
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
+
